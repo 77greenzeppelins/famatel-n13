be5be219070b0d79cb385ae80ad2d174bb16d051
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-01-28 13:01:14 +0100

    cards_newLayout_4.0

--- components/multipagesComponents/_categoryPageTemplate/subCategoriesCatalog/SubCategoriesCatalog.tsx
+++ components/multipagesComponents/_categoryPageTemplate/subCategoriesCatalog/SubCategoriesCatalog.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 /**Components**/
-import BasicCard from '../../cards/basicCard/BasicCard';
 /**TS**/
 import { IF_ImgStaticData } from '../../../../utils/TS/typeScriptStaff';
 import GridedCatalogLayout from '../../cardsCatalogs/_gridedCatalogLayout/GridedCatalogLayout';
@@ -17,15 +16,10 @@ const SubCategoriesCatalog: React.FunctionComponent<{
   };
 }> = ({ subCategoryData }) => {
   /**Props Destructurization*/
-  const { mainCategoryName, subCategoriesNames, subCategoriesUrls, imageData } =
-    subCategoryData;
+  const { subCategoriesNames, subCategoriesUrls, imageData } = subCategoryData;
 
   /**JSX**/
   return (
-    // <div
-    //   className="flex flex-wrap xl:justify-start gap-4 xl:gap-10"
-    // >
-
     <GridedCatalogLayout>
       {Array.from({ length: subCategoriesNames.length }).map((_, index) => (
         <Card_SubCategory
@@ -35,23 +29,23 @@ const SubCategoriesCatalog: React.FunctionComponent<{
           url={subCategoriesUrls[index]}
           imageData={imageData[index]}
         />
-        // <div
-        //   key={index}
-        //   data-layout="ParentContainerForCard"
-        //   className="relative w-[250px] h-[250px] lg:w-[300px] lg:h-[300px]"
-        // >
-        //   <BasicCard
-        //     // key={item}
-        //     arrayIndex={index}
-        //     label={subCategoriesNames[index]}
-        //     url={subCategoriesUrls[index]}
-        //     imageData={imageData[index]}
-        //   />
-        // </div>
       ))}
     </GridedCatalogLayout>
-    // </div>
   );
 };
 
 export default SubCategoriesCatalog;
+
+// <div
+//   key={index}
+//   data-layout="ParentContainerForCard"
+//   className="relative w-[250px] h-[250px] lg:w-[300px] lg:h-[300px]"
+// >
+//   <BasicCard
+//     // key={item}
+//     arrayIndex={index}
+//     label={subCategoriesNames[index]}
+//     url={subCategoriesUrls[index]}
+//     imageData={imageData[index]}
+//   />
+// </div>
