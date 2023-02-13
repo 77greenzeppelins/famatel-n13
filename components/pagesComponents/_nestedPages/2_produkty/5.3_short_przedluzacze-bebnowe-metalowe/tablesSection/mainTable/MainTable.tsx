cceff9f5ad4650a7d7c4e435fc1a567e7b709c0d
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-02-13 14:03:17 +0100

    productPages_tables_2.0

--- components/pagesComponents/_nestedPages/2_produkty/5.3_short_przedluzacze-bebnowe-metalowe/tablesSection/mainTable/MainTable.tsx
+++ components/pagesComponents/_nestedPages/2_produkty/5.3_short_przedluzacze-bebnowe-metalowe/tablesSection/mainTable/MainTable.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
 /**components**/
-import FixedTableBody_1 from '../../../../../../multipagesComponents/tables/fixedTable_1/body/FixedTableBody_1';
+import FixedTableBody_2 from '../../../../../../multipagesComponents/tables/fixedTable_2/body/FixedTableBody_2';
+/**Tailwind Styles**/
+import {
+  vertGap,
+  horizGap,
+  topHeaderCell,
+  sideHeaderCell,
+  bodyCell,
+} from '../../../../../../../utils/tailwindStyles';
+
 /**HardCoded Data*/
-const mainGridStyle =
-  'grid grid-cols-[1fr_1fr_2fr_2fr_1fr_1fr_1fr] divide-x-[2px] divide-dark';
+const mainGridStyle = `grid grid-cols-[1fr_1fr_2fr_2fr_1fr_1fr_1fr] `;
+const headerMainGrid = `${mainGridStyle} ${vertGap}`;
+const rowMainGrid = `${mainGridStyle} ${vertGap} group`;
 
 /**--------------------------------------------**/
 const MainTable: React.FunctionComponent<{
   tableHeader: string[];
   tableBody: (string | string[])[][];
 }> = ({ tableHeader, tableBody }) => {
-  /**Handler**/
-  const createTableBody = () => {
-    return tableBody.map((rowData, index) => (
-      <FixedTableBody_1
-        key={index}
-        rowData={rowData}
-        mainGridStyle={mainGridStyle}
-      />
-    ));
-  };
   /**JSX**/
   return (
-    <div className="flex flex-col divide-y-[2px] divide-dark">
-      {' '}
-      <div className={mainGridStyle}>
+    <div className={`flex flex-col ${horizGap}`}>
+      <div className={headerMainGrid}>
         {tableHeader.map((label, index) => (
-          <div className="fc bg-greyShade2 py-2 px-[2px]" key={index}>
+          <div key={index} className={topHeaderCell}>
             {' '}
-            <p className="p-small text-light text-center ">{label}</p>{' '}
+            <p>{label}</p>{' '}
           </div>
         ))}
       </div>
-      {createTableBody()}
+
+      <FixedTableBody_2
+        tableBody={tableBody}
+        rowMainGrid={rowMainGrid}
+        sideHeaderCell={sideHeaderCell}
+        bodyCell={bodyCell}
+      />
     </div>
   );
 };
