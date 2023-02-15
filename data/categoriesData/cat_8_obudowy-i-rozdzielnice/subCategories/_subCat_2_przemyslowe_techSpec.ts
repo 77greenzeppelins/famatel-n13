0fbca764216f291ce2d8db606c707c2adcddb00f
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-02-15 15:28:31 +0100

    rozdzielnice_tables_1.2

--- data/categoriesData/cat_8_obudowy-i-rozdzielnice/subCategories/_subCat_2_przemyslowe_techSpec.ts
+++ data/categoriesData/cat_8_obudowy-i-rozdzielnice/subCategories/_subCat_2_przemyslowe_techSpec.ts
@@ -668,11 +668,11 @@ tablesData: [
 data for specTable
 */
 const headerLabelStyle = 'text-[0.625rem]  xxl:text-[0.75rem] text-light';
-// const headerCellStyle = 'bg-greyShade1 opacity-[0.8] py-[0.3rem]';
-// const headerSocketCellStyle = 'bg-light opacity-[0.8] py-[0.3rem]';
-// const bodyLabelStyle =
-//   'text-[0.625rem]  xxl:text-[0.75rem] text-dark text-center';
-// const bodyCellStyle = 'py-[0.3rem] bg-greyTint2';
+const headerCellStyle = 'bg-greyShade1 opacity-[0.8] py-[0.3rem]';
+const headerSocketCellStyle = 'bg-light opacity-[0.8] py-[0.3rem]';
+const bodyLabelStyle =
+  'text-[0.625rem]  xxl:text-[0.75rem] text-dark text-center';
+const bodyCellStyle = 'py-[0.3rem] bg-greyTint2';
 const grid1colsStyle = 'grid grid-cols-[1fr] gap-[0.125rem]';
 const grid2colsStyle = 'grid grid-cols-[1fr_1fr] gap-[0.125rem]';
 const grid3colsStyle = 'grid grid-cols-[1fr_1fr_1fr] gap-[0.125rem]';
@@ -689,10 +689,10 @@ const gridTemplate8Columns =
 export {
   rozdzielnicePrzemysloweTechSpecData,
   headerLabelStyle,
-  // headerSocketCellStyle,
-  // headerCellStyle,
-  // bodyLabelStyle,
-  // bodyCellStyle,
+  headerSocketCellStyle,
+  headerCellStyle,
+  bodyLabelStyle,
+  bodyCellStyle,
   grid1colsStyle,
   grid2colsStyle,
   grid3colsStyle,
