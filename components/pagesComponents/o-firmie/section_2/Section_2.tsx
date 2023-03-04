2581539d898daf18c7a5f81536521a0f04adced0
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-04 17:04:29 +0100

    oFirmie_section2_1.0

--- components/pagesComponents/o-firmie/section_2/Section_2.tsx
+++ components/pagesComponents/o-firmie/section_2/Section_2.tsx
@@ -10,12 +10,12 @@ const Section_2: React.FunctionComponent<{
   return (
     <AnimatePresence>
       {isSection_2_Open && (
-        <motion.div className="bg-vY min-h-[50vh]">
-          <div className="hidden xl:flex w-full h-[10vh] bg-vB" />
-          {/* <ProductDescription
+        <motion.div className="min-h-[50vh]">
+          <div className="hidden xl:flex w-full h-[10vh]" />
+          <ProductDescription
             isProductDescriptionOpen={isSection_2_Open}
             productDescriptionOpener={setIsSection_2_Open}
-          /> */}
+          />
         </motion.div>
       )}
     </AnimatePresence>
