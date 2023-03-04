fc15892aec08e00cb9ea35bca52dc621af5ecdf4
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-04 16:25:15 +0100

    pseudoPanel_1.0

--- components/pagesComponents/o-firmie/section_1/graphicSection/imageSectin/ImageSection.tsx
+++ components/pagesComponents/o-firmie/section_1/graphicSection/imageSectin/ImageSection.tsx
@@ -58,7 +58,7 @@ const ImageSection: React.FunctionComponent<{
           height: size,
         }}
       >
-        {/* <PseudoTechPanel /> */}
+        <PseudoTechPanel />
         <AnimatedButton
           isSection_2_Open={isSection_2_Open}
           onClick={() => {
