fe4e6831f048e6d812fb32d21c420a9e830d64fb
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-04 17:00:57 +0100

    pseudoPanelTech_1.4

--- components/pagesComponents/o-firmie/section_1/graphicSection/imageSectin/ImageSection.tsx
+++ components/pagesComponents/o-firmie/section_1/graphicSection/imageSectin/ImageSection.tsx
@@ -58,7 +58,7 @@ const ImageSection: React.FunctionComponent<{
           height: size,
         }}
       >
-        <PseudoTechPanel />
+        <PseudoTechPanel isSection_2_Open={isSection_2_Open} />
         <AnimatedButton
           isSection_2_Open={isSection_2_Open}
           onClick={() => {
--- components/pagesComponents/o-firmie/section_1/graphicSection/pseudoTechPanel/PseudoTechPanel.tsx
+++ components/pagesComponents/o-firmie/section_1/graphicSection/pseudoTechPanel/PseudoTechPanel.tsx
@@ -1,28 +1,45 @@
+import { motion } from 'framer-motion';
 import React from 'react';
-/** */
+/**HardCoded Data**/
+//___delay to create a sort of "animation sequence" => image - overlay - sniper
+const delayForSniper = 3;
 
-const PseudoTechPanel = () => {
+const PseudoTechPanel: React.FunctionComponent<{
+  isSection_2_Open: boolean;
+}> = ({ isSection_2_Open }) => {
   return (
-    <div className="absolute hidden xs352:flex bottom-[10%] right-[25%] h-[70%] w-[70%]">
+    <motion.div
+      className="absolute hidden xs352:flex bottom-[10%] right-[25%] h-[70%] w-[70%]"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: delayForSniper, duration: 0.9 },
+      }}
+    >
       <div className="relative fc w-full h-full">
         <div className={` absolute top-0 left-0 h-[20%] w-[20%] `} />
         <div className={`absolute bottom-0 left-0 h-[50%] w-[50%] `}>
           <div className="relative flex items-end w-full h-full">
             <div className="w-full  flex flex-col pl-2">
-              <div className="flex items-center  ">
+              {/* <div className="flex items-center  ">
                 {' '}
                 <div className="fc w-[10px] h-[10px] bg-dark border-[1px] border-greyShade1"></div>{' '}
                 <div className="fc w-[10%] h-[1px] border-t-[1px] border-greyShade1"></div>{' '}
                 <div className="pl-2 ">
                   <p className="p-small text-greyShade1">IP 67</p>
                 </div>
-              </div>
-              <div className="flex items-center h-[30px]  ">
+              </div> */}
+              <div className="flex items-center h-[30px]">
                 {' '}
-                <div className="fc w-[10px] h-[10px] bg-dark border-[1px] border-greyShade1"></div>{' '}
+                {/* <div className="fc w-[10px] h-[10px] bg-dark border-[1px] border-greyShade1"></div>{' '}
                 <div className=" fc w-[15%] h-[1px] border-t-[1px] border-greyShade1"></div>{' '}
                 <div className="pl-2 ">
                   <p className="p-small text-greyShade1">125 A</p>
+                </div> */}
+                <div className="flex gap-1">
+                  <p className="p-small text-greyShade1">IP 67 /</p>
+                  <p className="p-small text-greyShade1">125 A /</p>
+                  <p className="p-small text-greyShade1">400 V</p>
                 </div>
               </div>
             </div>
@@ -36,7 +53,7 @@ const PseudoTechPanel = () => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
