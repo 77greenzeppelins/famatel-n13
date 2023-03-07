cfbe036ab439ec18f2d5662e1afc41d558d5b027
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-07 15:01:32 +0100

    mobileMenu_1.1

--- components/multipagesComponents/mobileMenu/mobileContactPanel/MobileContactPanel.tsx
+++ components/multipagesComponents/mobileMenu/mobileContactPanel/MobileContactPanel.tsx
@@ -25,30 +25,32 @@ const MobileContactPanel: React.FunctionComponent<{
         <motion.div
           key={isMobileMenuOpen.toString()}
           // data-layout="wrapper_for_DropDownMenuHolder"
-          className="fc fixed bottom-0 w-screen h-[100px] z-[500]"
+          className="fixed bottom-0 w-screen h-[60px] z-[500]"
           initial={{ opacity: 0, y: '100%' }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: '100%' }}
           transition={{ duration: 0.6, delay: 0.1, ease: 'easeOut' }}
         >
           <motion.div
-            //   className="absolute bottom-0 p-y-1 w-full h-[20%] "
-            className="w-full h-full"
+            className="w-full h-[50px]"
             initial={{ opacity: 0, y: '100%' }}
             animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
             exit={{ opacity: 0, y: '100%', transition: { duration: 1 } }}
           >
             <div className="w-full h-full grid grid-cols-[1fr_1fr_1fr] gap-[0.125rem]">
-              <div className="fc w-full h-full p-1">
+              <div
+                className="fc"
+                //___p-1
+              >
                 <motion.div
-                  className="fc w-full h-full border"
+                  className="fc aspect-square h-full border"
                   whileTap={{ scale: 0.95, borderColor: corpoColors.orange }}
                 >
                   <a
                     href="mailto:77greenzeppelins@gmail.com"
                     className="fc w-full h-full no-sparkling"
                   >
-                    <EnvelopeIcon className="h-[50px] w-[50px] text-grey" />
+                    <EnvelopeIcon className="h-[40px] w-[40px] text-grey" />
                   </a>
                 </motion.div>
               </div>
@@ -58,16 +60,16 @@ const MobileContactPanel: React.FunctionComponent<{
                   <MapPinIcon className="h-[50px] w-[50px] text-dark" />
                 </MapOpener> */}
               </div>
-              <div className="fc w-full h-full p-1">
+              <div className="fc ">
                 <motion.div
-                  className="fc w-full h-full border"
+                  className="fc aspect-square h-full border"
                   whileTap={{ scale: 0.95, borderColor: corpoColors.orange }}
                 >
                   <a
                     href="tel:798-905-558"
                     className="fc w-full h-full no-sparkling"
                   >
-                    <PhoneIcon className="h-[50px] w-[50px] text-grey" />
+                    <PhoneIcon className="h-[40px] w-[40px] text-grey" />
                   </a>
                 </motion.div>
               </div>
