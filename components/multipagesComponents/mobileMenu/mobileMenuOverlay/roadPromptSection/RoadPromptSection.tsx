d0942b82ebc59376ae5dec3c7ac0136d4d2bdc90
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-09 09:25:21 +0100

    mobileMenu_3.2

--- components/multipagesComponents/mobileMenu/mobileMenuOverlay/roadPromptSection/RoadPromptSection.tsx
+++ components/multipagesComponents/mobileMenu/mobileMenuOverlay/roadPromptSection/RoadPromptSection.tsx
@@ -14,11 +14,40 @@ const RoadPromptSection: React.FunctionComponent<{
       animate={{ x: roadPrompt ? 0 : '110%' }}
       transition={{ duration: 0.4, delay: 0.1, ease: 'easeOut' }}
     >
-      <div className="relative fc flex-col w-full h-full bg-dark">
-        <SvgMap containerStyle="width-full aspect-square" />
+      <div className="relative fc flex-col  gap-y-4 w-full h-full bg-dark pt-[50px] pb-[70px] ">
+        <div className="flex  flex-col w-[90%] ">
+          <div className="flex items-center w-full gap-1">
+            <p className="header-link-label text-grey">Adres</p>
+            <div className=" w-full h-[1px] border-t-[0.5px] gorder-grey " />
+          </div>
+          <div className="flex flex-col w-[90%] ">
+            <p className="p-small text-grey">ul.Willowa 5</p>
+            <p className="p-small text-grey">Bielawa</p>
+          </div>
+        </div>
+
+        <div className="flex  flex-col w-[90%] ">
+          <div className="flex items-center w-full gap-1">
+            <p className="header-link-label text-grey">Dojazd</p>
+            <div className=" w-full h-[1px] border-t-[0.5px] gorder-grey " />
+          </div>
+          <div className="flex flex-col w-[90%]">
+            <p className="p-small text-grey">ul.Strażacka</p>
+            <p className="p-small text-grey">Bielawa</p>
+            <p className="p-small text-grey">
+              coordynaty: 50.681392, 16.604118
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[90%]">
+          <SvgMap containerStyle="width-full aspect-square" />
+        </div>
       </div>
     </motion.div>
   );
 };
 
 export default RoadPromptSection;
+
+//___50.681392, 16.604118
