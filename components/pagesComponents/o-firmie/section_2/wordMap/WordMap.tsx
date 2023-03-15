c252d36cce62c2adebbfbbf14d7992e7862ae771
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-15 13:19:51 +0100

    draggableMap_1.0

--- components/pagesComponents/o-firmie/section_2/wordMap/WordMap.tsx
+++ components/pagesComponents/o-firmie/section_2/wordMap/WordMap.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+/**Components**/
 import SvgWordMap from '../../../../SVG/maps/SvgWordMap';
+/**Framer Motion Staff**/
+import { motion } from 'framer-motion';
+import useWindowSize from '../../../../../utils/hooks/useWindowSize';
 
+/**-----------------------**/
 const WordMap = () => {
+  /**...**/
+  const { width } = useWindowSize({ screensNumber: 1 });
+  const dragCondition = width <= 540;
+  /**JSX**/
   return (
-    <div className="w-full">
-      <SvgWordMap containerStyle="w-full" />
+    <div className="w-full overflow-hidden">
+      <motion.div
+        drag={dragCondition ? 'x' : false}
+        className="w-full min-w-[800px]"
+        dragConstraints={{ left: -400, right: 0 }}
+      >
+        <SvgWordMap containerStyle="w-full" />
+      </motion.div>
     </div>
   );
 };
