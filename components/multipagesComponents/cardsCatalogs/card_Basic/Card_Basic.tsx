520473b5246fb0239ecb949d49fa409a5fdabe6e
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-22 14:32:20 +0100

    categoriesCards_FromMenu_1.0

--- components/multipagesComponents/cardsCatalogs/card_Basic/Card_Basic.tsx
+++ components/multipagesComponents/cardsCatalogs/card_Basic/Card_Basic.tsx
@@ -6,6 +6,7 @@ import CardCounter from '../__cardCounter/CardCounter';
 import BasicCardMainSection from './mainSection/BasicCardMainSection';
 /**TS**/
 import { IF_Card } from '../../../../utils/TS/typeScriptStaff';
+import ArrowLongRightIcon from '../../../SVG/icons/ArrowLongRightIcon';
 /*
 used in: manu expanded menu / catalog....
 */
@@ -26,8 +27,13 @@ const Card_Basic: React.FunctionComponent<IF_Card> = ({
       <Link href={url} scroll={false}>
         <CardFrame>
           <div className="relative fc flex-col xs:flex-row w-full h-full ">
-            <div className="fc w-full h-[40%] xs:w-[18%] xs:h-[80%]">
-              <CardCounter arrayIndex={arrayIndex} />
+            <div className="fc w-full h-[40%] xs:w-[18%] xs:h-[80%] ">
+              <div className="fc w-[50%] h-[50%]  xs:h-full">
+                <CardCounter arrayIndex={arrayIndex} />
+              </div>
+              <div className="fc xs:hidden w-[50%] h-full ">
+                <ArrowLongRightIcon containerStyle="fc h-6 w-6 aspect-square stroke-greyShade1 group-hover:stroke-light group-hover:translate-x-1 ease-in duration-[0.4s] delay-[0.1s] origin-center " />
+              </div>
             </div>
             <div className="w-full h-[60%] xs:w-[82%] xs:h-full">
               <BasicCardMainSection
