7d691a442c9796dcda980ffa41d171555c8935f4
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-03-24 05:52:39 +0100

    downloadableLink_1.3

--- components/pagesComponents/doPobrania/catalogChapters/chapterLine/ChapterLine.tsx
+++ components/pagesComponents/doPobrania/catalogChapters/chapterLine/ChapterLine.tsx
@@ -18,36 +18,47 @@ const ChapterLine: React.FC<{ chapterName: string; chapterNumber: number }> = ({
   /**JSX**/
 
   return (
-    <div className="relative min-h-[32px] flex items-center group">
+    <div className="relative min-h-[64px] sm:min-h-[32px] flex items-center">
       <div className="min-w-[24px]">
         <p className={textStyle}>{chapterNumber}</p>
       </div>
-      <div className="flex xs:flex-row flex-col w-full justify-between flex-wrap">
-        <div className="xs3xx:min-w-[312px] ">
+      <div className="flex flex-col gap-y-2 sm:flex-row  w-full justify-between flex-wrap">
+        <div className="sm3xx:min-w-[312px] ">
           <p className={textStyle}>{chapterName}</p>
         </div>
-        <div className="flex gap-6">
-          <div className="flex items-center gap-6 group">
+        <div className="flex gap-9">
+          <div className="flex items-center gap-3 group">
             <ArrowDownloadIcon
               className={
-                'w-6 h-6 stroke-light group-hover:translate-y-1 ease-in duration-300 delay-300'
-                //___animate-bounce animate-ping group-hover:translate-y-1
+                'w-4 h-4 sm:w-6 sm:h-6 stroke-grey group-hover:stroke-light group-hover:translate-y-1 ease-in duration-300 delay-300'
+                //___animate-bounce animate-ping group-hover:translate-y-1 group-hover:translate-x-1 group-hover:scale-[1.1]
               }
             />
             <DownloadableLink
               staffToDownload={
                 '/pdf/catalogChapters/05_przedluzacze_bebnowe.pdf'
               }
-              linkLabel={'100 mb'}
+              linkLabel={'150 mb'}
+              textStyle={textStyle}
+            />
+          </div>
+          <div className="flex items-center gap-3 group">
+            <ArrowDownloadIcon
+              className={
+                'w-4 h-4 sm:w-6 sm:h-6 stroke-grey group-hover:stroke-light group-hover:translate-y-1 ease-in duration-300 delay-300'
+                //___animate-bounce animate-ping group-hover:translate-y-1 group-hover:translate-x-1 group-hover:scale-[1.1]
+              }
+            />
+            <DownloadableLink
+              staffToDownload={
+                '/pdf/catalogChapters/05_przedluzacze_bebnowe.pdf'
+              }
+              linkLabel={'25 mb'}
               textStyle={textStyle}
             />
           </div>
         </div>
       </div>
-      {/* <div className="flex gap-6">
-        <p className="text text-grey">{chapterName}</p>
-        <ArrowDownloadIcon className={'w-6 h-6'} />
-      </div> */}
       <div className="absolute inset-0 flex border-b border-greyShade2 group-hover:border-grey ease-in duration-300 pointer-events-none" />
     </div>
   );
