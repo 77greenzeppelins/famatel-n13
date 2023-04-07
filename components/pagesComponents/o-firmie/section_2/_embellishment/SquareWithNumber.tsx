84668778226c655dce7dd9de7435ef69718be72e
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-04-07 14:13:29 +0200

    oFirmiePage_adjustments_2.1

--- components/pagesComponents/o-firmie/section_2/_embellishment/SquareWithNumber.tsx
+++ components/pagesComponents/o-firmie/section_2/_embellishment/SquareWithNumber.tsx
@@ -21,37 +21,76 @@ const SquareWithNumber: React.FC<Props> = ({
   return (
     <div
       className={`fc flex-col  ${
-        componentIsInView ? 'bg-corpo translate-x-0' : 'bg-dark translate-x-4'
-      } duration-[0.9s] delay-[0.6] p-2 disable-soft w-[80px] h-[80px] md:w-[120px] md:h-[120px]`}
-      style={{
-        //  width: val,
-        aspectRatio: '1/1',
-      }}
+        componentIsInView
+          ? 'bg-greyShade2 translate-x-0'
+          : 'bg-dark translate-x-4'
+      } duration-[0.9s] delay-[0.6] p-2 disable-soft w-[80px] h-[80px] md:w-[120px] md:h-[120px] aspect-square`}
+      // style={{
+      //   //  width: val,
+      //   aspectRatio: '1/1',
+      // }}
     >
       <p
         // ref={ref}
         className={` text-center text-[1.25rem] xs:text-[1.5rem]  md:text-[1.75rem] lg:text-[2rem] xl:text-[2.25rem]   font-bold tracking-[2px] leading-tight  ${
-          componentIsInView ? 'text-light' : 'text-dark'
+          componentIsInView ? 'text-corpo' : 'text-dark'
         } duration-[0.9s] delay-[0.6] pb-1`}
-        style={{ width: 'content-fit' }}
+        // style={{ width: 'content-fit' }}
       >
         {pseudoNumber}
       </p>
       <div
-        className={`w-full h-[1px] ${
-          componentIsInView ? 'bg-light ' : 'bg-dark '
+        className={`w-full h-[1px] border-t ${
+          componentIsInView ? 'border-corpo ' : 'border-dark '
         } duration-[0.9s] delay-[0.6]`}
       />
       <p
         className={`header-link-label text-center ${
-          componentIsInView ? 'text-light' : 'text-dark'
-        } duration-[0.9s] delay-[0.6]`}
-        style={{ width: 'content-fit' }}
+          componentIsInView ? 'text-corpo' : 'text-dark'
+        } duration-[0.9s] delay-[0.6] pt-[2px]`}
+        // style={{ width: 'content-fit' }}
       >
         {text}
       </p>
     </div>
   );
+
+  // return (
+  //   <div
+  //     className={`fc flex-col border  ${
+  //       componentIsInView
+  //         ? 'border-corpo translate-x-0'
+  //         : 'border-dark translate-x-4'
+  //     } duration-[0.9s] delay-[0.6] p-2 disable-soft w-[80px] h-[80px] md:w-[120px] md:h-[120px] aspect-square`}
+  //     // style={{
+  //     //   //  width: val,
+  //     //   aspectRatio: '1/1',
+  //     // }}
+  //   >
+  //     <p
+  //       // ref={ref}
+  //       className={` text-center text-[1.25rem] xs:text-[1.5rem]  md:text-[1.75rem] lg:text-[2rem] xl:text-[2.25rem]   font-bold tracking-[2px] leading-tight  ${
+  //         componentIsInView ? 'text-light' : 'text-dark'
+  //       } duration-[0.9s] delay-[0.6] pb-1`}
+  //       // style={{ width: 'content-fit' }}
+  //     >
+  //       {pseudoNumber}
+  //     </p>
+  //     <div
+  //       className={`w-full h-[1px] border-t ${
+  //         componentIsInView ? 'border-corpo ' : 'border-dark '
+  //       } duration-[0.9s] delay-[0.6]`}
+  //     />
+  //     <p
+  //       className={`header-link-label text-center ${
+  //         componentIsInView ? 'text-light' : 'text-dark'
+  //       } duration-[0.9s] delay-[0.6] pt-[2px]`}
+  //       // style={{ width: 'content-fit' }}
+  //     >
+  //       {text}
+  //     </p>
+  //   </div>
+  // );
 };
 
 export default SquareWithNumber;
