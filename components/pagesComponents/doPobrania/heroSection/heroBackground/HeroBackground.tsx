2145d4c977869617732fb959996ed544dcc3a8c3
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-04-11 10:31:35 +0200

    doPobrania_heroBackgroudAnimation_1.2

--- components/pagesComponents/doPobrania/heroSection/heroBackground/HeroBackground.tsx
+++ components/pagesComponents/doPobrania/heroSection/heroBackground/HeroBackground.tsx
@@ -7,6 +7,7 @@ import {
   useTransform,
   cubicBezier,
   easeIn,
+  useMotionValueEvent,
 } from 'framer-motion';
 import InViewContainer from '../../../../containers/inViewContainer/InViewContainer';
 import InViewAnimatedContent from '../../../kontakt/_inViewAnimatedContent/InViewAnimatedContent';
@@ -20,17 +21,25 @@ import useMeasure from 'react-use-measure';
 const heightValue = 668;
 const svgBasicSize = 220;
 const svgContainerSizes = 'w-[220px] h-[220px]';
+const springOptions = {
+  damping: 100,
+  mass: 10,
+  stiffness: 100,
+};
 
 /**----------------------------**/
 const HeroBackground = () => {
-  const scrollRef = useRef(null);
+  const scrollRef = useRef<number>(0);
+
   /**...**/
   const [ref, { height }] = useMeasure();
   /**FramerMotion Staff**/
-  const { scrollYProgress, scrollY } = useScroll({
-    target: scrollRef,
-    // offset: ['start end', 'end end'],
-  });
+  const { scrollYProgress, scrollY } = useScroll();
+  //   {
+  //   target: scrollRef,
+  //   offset: ['end end', 'start start'],
+  // }
+
   const xLeft = useTransform(scrollYProgress, [0, 1], [0, -500]);
   //___, {ease: easeIn,}
   const xRight = useTransform(scrollYProgress, [0, 1], [0, 500]);
@@ -47,6 +56,18 @@ const HeroBackground = () => {
   // }
   // { easeIn }
 
+  const xDivided = useTransform(x, value => value / 2);
+
+  useMotionValueEvent(scrollY, 'change', latest => {
+    console.log('Page scroll: ', latest);
+    scrollRef.current = latest / 2;
+    console.log('scrollRef.current: ', latest / 2);
+  });
+
+  //____
+
+  // let d = 50;
+  //___
   /**JSX**/
   return (
     <div
@@ -54,7 +75,10 @@ const HeroBackground = () => {
       ref={ref}
       className="absolute hidden md:block inset-0 overflow-hidden z-5"
     >
-      <div className="relative w-full h-full" ref={scrollRef}>
+      <div
+        className="relative flex flex-col  justify-around w-full h-full"
+        // ref={scrollRef}
+      >
         {/* <InViewContainer
           animationDelay={0.1}
           outherContainerStyle="w-full h-full "
@@ -84,9 +108,15 @@ const HeroBackground = () => {
           className="flex w-full justify-end"
         >
           <motion.div
-            className="flex gap-4 transition-all ease-in-out"
+            className="flex gap-4 "
+            //___transition-all ease-in-out
+            // style={{ x }}
+            style={{ x: xDivided }}
+            // style={{ transform: `translateX(${x / 2}px)` }}
             // style={{ x: xLeft }}
-            style={{ x }}
+            // style={{ transform: `translateX(${xx}px)` }}
+            // initial={{ x: 0 }}
+            // animate={{ x: scrollRef.current }}
             transition={{ stiffness: 100, damping: 30, restDelta: 0.001 }}
           >
             {svgTech_darkBackground2.map(({ model, Component }, i) => (
