12b7b047a0264c3fc7a45fea5d468c249dfa3f46
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-04-27 06:12:09 +0200

    uninstalled_locomotive-scroll_framer-motion-carousel

--- components/pagesComponents/o-firmie/section_2/productDescription/ProductDescription.tsx
+++ components/pagesComponents/o-firmie/section_2/productDescription/ProductDescription.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
 /**Components**/
 import CatalogNavPanel from '../../../../multipagesComponents/navigations/catalogNavPanel/CatalogNavPanel';
-import SVG_14307_P28 from '../../../../SVG/techDrawings/1_wtyczki-gniazda/SVG_14307_P28';
+import SVG_14307_d from '../../../../SVG/techDrawings/0_dark/SVG_14307_d';
 /**Framer Motion Staff*/
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 /** */
 import { catalogStructureData } from '../../../../../data/_catalogStructure_data';
 import { smallPseudoHeaders } from '../../../../../data/_data';
 
-// interface Props {
-//   isProductDescriptionOpen: boolean;
-//   productDescriptionOpener: React.Dispatch<React.SetStateAction<boolean>>;
-// }
 /**-------------------------------------------------------**/
 const ProductDescription: React.FC = () => {
   /**...**/
 
   /**JSX**/
   return (
-    // <AnimatePresence>
-    //   {isProductDescriptionOpen && (
     <motion.div
       data-component="ProductDescription__container"
-      // className="absolute inset-0 px-[2%] xxs:px-[6%] md:px-[60px] xl:px-0 xl:pr-[80px] xxl:pr-[100px] xl:pt-[70px] bg-dark "
-      //___mix of inner-pr-md-xl-xxl & inner-px-md-xl-xxl
       className="bg-dark"
       initial={{ opacity: 0, scale: 1 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ delay: 0.2, duration: 0.4 }}
     >
-      <div
-        className="flex flex-col gap-y-8 xl:flex-row-reverse xl:gap-y-0 xl:gap-x-8 xxl:gap-x-20 inner-px-md-xl-xxl"
-        // className="relative flex justify-center flex-col xl:flex-col  gap-y-2 w-full h-full  mt-[40px] xs:mt-[80px] md:mt-[60px] lg:mt-[180px]  xl:items-start xl:justify-center xl:mt-0 "
-      >
+      <div className="flex flex-col gap-y-8 xl:flex-row-reverse xl:gap-y-0 xl:gap-x-8 xxl:gap-x-20 inner-px-md-xl-xxl">
         <div className=" flex flex-col justify-center items-start gap-y-8 w-full xl:w-[50%] ">
           <CatalogNavPanel
             // key={index}
@@ -48,68 +37,11 @@ const ProductDescription: React.FC = () => {
           />
         </div>
         <div className="flex justify-start bg-dark w-full max-w-[700px] lg:max-w-[1000px] xl:max-w-full">
-          <SVG_14307_P28 />
+          <SVG_14307_d />
         </div>
       </div>
     </motion.div>
-    //   )}
-    // </AnimatePresence>
   );
 };
 
 export default ProductDescription;
-
-{
-  /* <div className="absolute right-0 w-6 h-6 -top-6 xs:top-0 fc xl:relative xl:flex xl:justify-end xl:w-full">
-              <CloseButton
-                productDescriptionOpener={productDescriptionOpener}
-                buttonStyle="relative fc focus:outline-none disable pointer-events-auto group"
-              />
-            </div> */
-}
-
-{
-  /**HardCodedData**/
-  // const links = [
-  //   {
-  //     l1: 'Kategoria',
-  //     l2: 'Przemysłowe wtyczki i gniazda',
-  //     linkHref: catalogStructureData[0].mainCategoryUrl,
-  //   },
-  //   {
-  //     l1: 'Podkategoria',
-  //     l2: 'Przenośne',
-  //     linkHref: catalogStructureData[0].subCategoriesUrls[0],
-  //   },
-  //   {
-  //     l1: 'Model',
-  //     l2: '14307',
-  //     linkHref: `${catalogStructureData[0].subCategoriesUrls[0]}/wtyczki-ip67-125A`,
-  //   },
-  // ];
-  /* {links.map(({ l1, l2, linkHref }, i) => (
-                <div
-                  key={i}
-                  className={`${
-                    i === 1
-                      ? 'hidden xxs:flex flex-col group'
-                      : 'xxs:flex flex-col group'
-                  }`}
-                >
-                  <SmallPseudoHeader
-                    text={l1}
-                    hasBox={true}
-                    hasVerticalOrnament={false}
-                  />
-                  <LinkWithTextAndIcon
-                    linkHref={linkHref}
-                    pLabel={l2}
-                    // mainContainerStyle="w-[90%]"
-                    aStyle="fc gap-4 h-full  bg-transparent focus:outline-none "
-                    hasBox={true}
-                    hasVerticalOrnament={false}
-                    pStyle="flex items-center h-full p-medium text-grey group-hover:text-light ease-in duration-[0.4s] delay-[0.1s]"
-                  />
-                </div>
-              ))} */
-}
