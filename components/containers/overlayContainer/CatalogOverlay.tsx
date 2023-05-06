2ef8f6893c767664faa9630faf0720db53fb7959
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-05-06 15:09:10 +0200

    s2_del_DropDownMenuHolder_2.1

--- components/containers/overlayContainer/CatalogOverlay.tsx
+++ components/containers/overlayContainer/CatalogOverlay.tsx
@@ -6,6 +6,19 @@ import { globalState } from '../../../globalState/globalState';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import ProduktyDropDownMenu from '../../layouts/rootLayout/header/dropDownCatalog/produktyDropDownMenu/ProduktyDropDownMenu';
+import { overlayContainerVariants } from '../../../utils/framerMotion/framerMotionUtils';
+import { catalogStructureData } from '../../../data/_catalogStructure_data';
+import Card_Basic from '../../multipagesComponents/cardsCatalogs/card_Basic/Card_Basic';
+import SpecialCard from '../../layouts/rootLayout/header/dropDownCatalog/produktyDropDownMenu/specialCard/SpecialCard';
+
+/**Tailwind**/
+const basicTextStyle =
+  ' text-center text-[0.5rem] xs3xx:text-[0.625rem] md:text-[0.75rem] xxxl:text-[1.125rem] tracking-[0.09rem] leading-normal group-hover:text-light ease-in duration-300 ';
+const normalCardTextStyle = `text-grey ${basicTextStyle}`;
+const specialCardTextStyle = `text-greyShade2 ${basicTextStyle}`;
+
+const largeTextStyle =
+  'text-greyShade2 text-1xl sm:text-2xl xl:text-3xl tracking-[0.09rem]';
 
 /**TS**/
 interface State {
@@ -39,15 +52,55 @@ const CatalogOverlay = () => {
   return (
     <AnimatePresence mode="wait">
       {snap.isCatalogOpen && (
-        <div
+        <motion.div
           data-layout="wrapper_for_ProduktyDropDownMenu"
           className="fixed flex justify-center left-0 right-0 top-[50px] bottom-[1px] pointer-events-none "
+          key={snap.isCatalogOpen.toString()}
+          variants={overlayContainerVariants}
+          animate="animate"
+          initial="initial"
+          exit="initial"
         >
-          <ProduktyDropDownMenu
-            isHovered={snap.isCatalogOpen}
-            hasDropDownMenu={true}
-          />
-        </div>
+          <div
+            data-component="ProduktyDropDownMenu__container"
+            className="fc w-screen h-screen bg-dark pointer-events-auto"
+          >
+            <div className={`w-[98%] h-[80%] xl:h-[90%] fc bg-dark`}>
+              <div
+                data-layout="ProduktyDropDownMenu__griddedContainer"
+                className="grid grid-rows-4 grid-cols-3 xs:grid-rows-3 xs:grid-cols-4 w-[100vw] h-full pt-[24px] pb-[4rem] px-[1rem] gap-[0.75rem]"
+                //___place-items-stretch
+                //className="grid grid-rows-4 grid-cols-3 xs:grid-rows-3 xs:grid-cols-4 w-full h-full pt-[24px] pb-[4rem] px-[1rem] gap-[0.75rem]"
+              >
+                {catalogStructureData.map(
+                  ({
+                    mainCategoryIndex,
+                    mainCategoryName,
+                    mainCategoryUrl,
+                    mainCategoryImage,
+                  }) => (
+                    <Card_Basic
+                      key={mainCategoryIndex}
+                      url={mainCategoryUrl}
+                      imageData={mainCategoryImage}
+                      arrayIndex={mainCategoryIndex}
+                      label={mainCategoryName}
+                      heightMattersCondition={true}
+                      textStyle={normalCardTextStyle}
+                    />
+                  )
+                )}
+
+                <div className="col-start-2 xs:col-start-3 col-span-full ">
+                  <SpecialCard
+                    mediumTextStyle={specialCardTextStyle}
+                    largeTextStyle={largeTextStyle}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
