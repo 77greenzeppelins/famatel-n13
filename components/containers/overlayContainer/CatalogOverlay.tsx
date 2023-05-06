d6b1c8dde61b0bfd2638b91a26536fcf2f16f57a
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-05-06 12:11:06 +0200

    header2_catalogOverlay_1.3

--- components/containers/overlayContainer/CatalogOverlay.tsx
+++ components/containers/overlayContainer/CatalogOverlay.tsx
@@ -15,7 +15,7 @@ const CatalogOverlay = () => {
       {snap.isCatalogOpen ? (
         <motion.div
           data-component="CatalogOverlay"
-          className="fixed left-0 right-0 top-0 bottom-0 z-[490] pt-[50px] bg-greyShade2"
+          className="fixed left-0 right-0 top-[40px] bottom-0 z-[490]  bg-greyShade2"
           initial={{ x: '100%' }}
           animate={{
             x: 0,
