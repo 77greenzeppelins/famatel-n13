b24dfc4f2972d50c96809811cb80011fc4025c0f
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-05-06 12:18:04 +0200

    header2_catalogOverlay_1.4

--- components/containers/overlayContainer/CatalogOverlay.tsx
+++ components/containers/overlayContainer/CatalogOverlay.tsx
@@ -11,11 +11,12 @@ const CatalogOverlay = () => {
   const snap = useSnapshot(globalState);
   /**JSX*/
   return (
-    <AnimatePresence>
-      {snap.isCatalogOpen ? (
+    <AnimatePresence mode="wait">
+      {snap.isCatalogOpen && (
         <motion.div
-          data-component="CatalogOverlay"
-          className="fixed left-0 right-0 top-[40px] bottom-0 z-[490]  bg-greyShade2"
+          data-component="MobileMenuOverlay__container"
+          key={snap.isCatalogOpen.toString()}
+          className="fixed left-0 right-0 top-0 bottom-0 z-[490] bg-grey"
           initial={{ x: '100%' }}
           animate={{
             x: 0,
@@ -27,11 +28,35 @@ const CatalogOverlay = () => {
             transition: { duration: 0.8, delay: 1, ease: 'easeOut' },
           }}
         >
-          <ProduktyDropDownMenu />
+          <div className="relative fc w-full h-full bg-grey inner-px-md-xl-xxl">
+            <ProduktyDropDownMenu />
+          </div>
         </motion.div>
-      ) : null}
+      )}
     </AnimatePresence>
   );
+  // return (
+  //   <AnimatePresence>
+  //     {snap.isCatalogOpen ? (
+  //       <motion.div
+  //         data-component="CatalogOverlay"
+  //         className="fixed left-0 right-0 top-[40px] bottom-0 z-[490]  bg-greyShade2"
+  //         initial={{ x: '100%' }}
+  //         animate={{
+  //           x: 0,
+  //           transition: { duration: 0.6, delay: 0.1, ease: 'easeOut' },
+  //         }}
+  //         exit={{
+  //           opacity: 0.9,
+  //           x: '100%',
+  //           transition: { duration: 0.8, delay: 1, ease: 'easeOut' },
+  //         }}
+  //       >
+  //         <ProduktyDropDownMenu />
+  //       </motion.div>
+  //     ) : null}
+  //   </AnimatePresence>
+  // );
 };
 
 export default CatalogOverlay;
