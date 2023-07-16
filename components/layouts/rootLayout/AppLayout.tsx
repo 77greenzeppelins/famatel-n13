7ca98c166ef9145f0938c41b4d57bd4942cdef6c
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-16 10:14:19 +0200

    Header_3.1

--- components/layouts/rootLayout/AppLayout.tsx
+++ components/layouts/rootLayout/AppLayout.tsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
-/**Components*/
+import React from 'react';
+/**Components**/
 import Header from './header/Header';
-import MobileMenuButton from '../../multipagesComponents/mobileMenu/mobileMenuButton/MobileMenuButton';
-import MobileMenuOverlay from '../../multipagesComponents/mobileMenu/mobileMenuOverlay/MobileMenuOverlay';
-import MobileContactPanel from '../../multipagesComponents/mobileMenu/mobileContactPanel/MobileContactPanel';
 import Footer from './footer/Footer';
 import CookiesPopUp from './cookiesPopUp/CookiesPopUp';
-/**Basic Data**/
-import { mobileMenuData } from '../../../data/_data';
 /**Font Staff**/
 import localFont from '@next/font/local';
 import MobileMenu from '../../multipagesComponents/mobileMenu/MobileMenu';
@@ -18,15 +13,6 @@ const haasFont = localFont({
 
 /**-----------------------------------------------------------------------------**/
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  /**Local States**/
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [roadPrompt, setRoadPrompt] = useState(false);
-
-  // useEffect(() => {
-  //   console.log('isMobileMenuOpen', isMobileMenuOpen);
-  //   console.log('roadPrompt', roadPrompt);
-  // }, [isMobileMenuOpen, roadPrompt]);
-
   /**JSX**/
   return (
     <div
@@ -36,24 +22,6 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <Header />
       {children}
       <MobileMenu />
-
-      {/* <MobileMenuButton
-        mobileMenuOpener={setIsMobileMenuOpen}
-        mobileMenuState={isMobileMenuOpen}
-      />
-      <MobileMenuOverlay
-        isMobileMenuOpen={isMobileMenuOpen}
-        mobileMenuOpener={setIsMobileMenuOpen}
-        setRoadPrompt={setRoadPrompt}
-        roadPrompt={roadPrompt}
-      />
-      <MobileContactPanel
-        isMobileMenuOpen={isMobileMenuOpen}
-        roadPrompt={roadPrompt}
-        setRoadPrompt={setRoadPrompt}
-        maxW={mobileMenuData.maxW}
-        minH={mobileMenuData.minH}
-      /> */}
 
       <Footer />
       <CookiesPopUp />
--- components/layouts/rootLayout/header/navSection/headerLink/HeaderLink.tsx
+++ components/layouts/rootLayout/header/navSection/headerLink/HeaderLink.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -18,37 +19,50 @@ const HeaderLink: React.FC<Props> = ({ label, url }) => {
   why: for style sake;  I want link to be in corpo color and has no border-bottom when user is on its corresponding page
   */
   const router = useRouter();
-  const linkStyleisHovered = router.pathname === url;
   const isActive = router.asPath === url;
 
   /**JSX*/
   return (
     <>
-      <li data-component="NavLink_container" className="relative ml-6 fc">
-        <div className="hidden lg:block">
-          <Link
-            href={url}
-            aria-label={`Link do strony ${label}`}
-            aria-current={isActive ? 'page' : undefined}
-            scroll={false}
-            className="relative fc gap-1 w-full h-full cursor-pointer bg-transparent select-none touch-none  group"
+      <li
+        data-component="NavLink_container"
+        className="relative ml-6 fc hidden lg:block"
+      >
+        <Link
+          href={url}
+          aria-label={`Link do strony ${label}`}
+          aria-current={isActive ? 'page' : undefined}
+          scroll={false}
+          // className="relative fc gap-1 w-full h-full cursor-pointer bg-transparent select-none touch-none  group"
+          className={`relative text-light header-link-label disable h-[40px] fc  ${
+            isActive ? 'hover:text-light' : 'hover:text-corpo'
+          }  delay-100 duration-300 ease-linear`}
+        >
+          {/* <span
+            className={`header-link-label ${
+              linkStyleisHovered ? 'text-corpo' : 'text-grey'
+            }`}
           >
-            <span
-              className={`header-link-label ${
-                linkStyleisHovered ? 'text-corpo' : 'text-grey'
-              }`}
-            >
-              <span>{label}</span>
-            </span>
-            <span
-              className={`h-[1px] inline-block w-0 bg-grey absolute left-0 -bottom-[2px] group-hover:w-full transition-[width] ease-in duration-300 delay-400 ${
-                linkStyleisHovered ? 'opacity-0' : 'opacity-1'
-              }`}
-            >
-              &nbsp;
-            </span>
-          </Link>
-        </div>
+            <span>{label}</span>
+          </span>
+          <span
+            className={`h-[1px] inline-block w-0 bg-grey absolute left-0 -bottom-[2px] group-hover:w-full transition-[width] ease-in duration-300 delay-400 ${
+              linkStyleisHovered ? 'opacity-0' : 'opacity-1'
+            }`}
+          >
+            &nbsp;
+          </span> */}
+          {isActive && (
+            <motion.span
+              className="absolute bottom-[8px] block h-[1px] bg-corpo origin-center "
+              initial={{ width: 0 }}
+              animate={{ width: '100%' }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+            />
+          )}
+
+          {label}
+        </Link>
       </li>
     </>
   );
