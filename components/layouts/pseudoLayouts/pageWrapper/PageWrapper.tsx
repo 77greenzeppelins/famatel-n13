ef8a1863fb23708cfae4144fdbbdec8271038e46
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-07-28 09:20:53 +0200

    AnimatePresence_PageWrapper_1.1

--- components/layouts/pseudoLayouts/pageWrapper/PageWrapper.tsx
+++ components/layouts/pseudoLayouts/pageWrapper/PageWrapper.tsx
@@ -9,12 +9,6 @@ interface Props {
   children: React.ReactNode;
 }
 
-const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
-};
-
 const PageWrapper = ({ children }: Props) => {
   const router = useRouter();
 
